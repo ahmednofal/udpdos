81de6a2e4c778cf1761b427a872cb475fb4406a6
naufal <ahmednofal@aucegypt.edu>
2018-09-02 12:51:13 +0200

    saving for deployment

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+tool_name="udpdos"
+version_num = "0.0.1"
+author_name = "Hedi Nasr"
+setuptools.setup(name=tool_name,
+        version=version_num,
+        author=author_name,
+        description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://gist.github.com/Ananasr/e05f3286b6ab94ec2c5431e64832c13e",
+        packages=setuptools.find_packages(),
+        classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+        scripts = [
+                'core/udpdos'
+            ],
+)
